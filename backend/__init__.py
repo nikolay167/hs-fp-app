87bac3123f9c495d6889c75e6c5fddbfcc82c83f
nikolay167 <nikolay@shityakov.com>
2017-10-30 20:05:28 +0100

    test2

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,13 +1,12 @@
 #query_string='Alex went to the rest'
 def process_user_query(query_string):
-    words = []
-for line in query_string:
-    for word in line.split():
-        if word[0].isupper():
-            words.append(word)
+    for line in query_string:
+        words = []
+        for word in line.split():
+            if word[0].isupper():
+                return(words.append(word)
 
-for word in sorted(set(words)):
-    return(word)
+
 
     #split_func=query_string.split()
     #greetings=[i for i in split_func]
