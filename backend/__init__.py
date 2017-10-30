c3ba7a95839133c57301f200ae1b1e89160c07e6
nikolay167 <nikolay@shityakov.com>
2017-10-30 14:18:29 +0100

    3

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,5 @@
 def process_user_query(query_string):
-    split_func=query_string.split()
+    split_func=query_string.split(end=',')
+
     for i in split_func:
-        return(f'HI! + {i}')
+    return(f'HI!  {i}')
