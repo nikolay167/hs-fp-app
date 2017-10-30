93a89347f644d0360a80f14b9e4c19776a356255
nikolay167 <nikolay@shityakov.com>
2017-10-30 14:36:23 +0100

    3

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,6 @@
 
 def process_user_query(query_string):
-    split_func=query_string.split()
-    greetings=[]
-    for i in split_func:
-        greetings.append(f'HI!  {i}')
-    return greetings
+    #split_func=query_string.split()
+    #greetings=[i for i in split_func]
+    #return greetings
+    return [f'HI{name}!' for name in query_string()]
