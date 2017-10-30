ab70785de8ad22b2c071b8bf196f5d43ab0efd9e
nikolay167 <nikolay@shityakov.com>
2017-10-30 20:44:07 +0100

    hm maybe working code dont you think so ?

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,23 @@
 #query_string='Alex went to the rest'
-def process_user_query(query_string):
-    for line in query_string:
-        words = []
-        for word in line.split():
-            if word[0].isupper():
-                return(words.append(word))
+query_string='Alex is my Best Friend '
+words = ['Nikolay', 'Nishit', 'Alex', 'Catalina', 'Leo', 'OVER', 'the', 'Lazy', 'DOG']
+while True:
+    def process_user_query(query_string):
+        try :
+            for word in query_string.split():
+                for i in words:
+                    if word==i:
+                        return (f'Hi {word}')
 
+
+#def process_user_query(query_string):
+
+        #for line in query_string:
+        #words = []
+        #for word in line.split():
+            #if word[0].upper():
+            #    return(words.append(word))
+            #    print(words)
 
 
     #split_func=query_string.split()
