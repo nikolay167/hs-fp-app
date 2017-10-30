072493a2d5e83f3f7f4955ea3fa9a32ff81eaed2
nikolay167 <nikolay@shityakov.com>
2017-10-30 14:02:55 +0100

    g

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,3 +1,7 @@
 def process_user_query(query_string):
-
-    return(f'Hello there! {query_string}!')
+    if len(student_names) == 0:
+        return 'No studets '
+    result= ''
+    for name in query_string:
+        result = result + ' ' +name
+    return result
