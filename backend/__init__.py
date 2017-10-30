88f34b0d670457c33c2440cfcf8d7f78f66c402e
nikolay167 <nikolay@shityakov.com>
2017-10-30 14:07:09 +0100

    3

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,3 @@
 def process_user_query(query_string):
-    if len(query_string) == 0:
-        return 'No studets '
-    result= ''
-    for name in query_string:
-        result = result + ' ' +name
-    return result
+    split_func=query_string.split()
+    return(f'Hello! {split_func}!')
