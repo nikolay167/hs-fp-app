d4415082c6d100965cdb835b5115938b0901a1a4
nikolay167 <nikolay@shityakov.com>
2017-10-31 15:52:40 +0100

    test

--- backend/__init__.py
+++ backend/__init__.py
@@ -12,5 +12,4 @@ def process_user_query(query_string):
 
 # gentleman's rule: if you opened something, and don’t need this any more, then close it!
     f.close()
-    re.compile(r'href *= *"([^"]*)"').findall(web_page_contents)[:10]
-    return (re.compile)
+    return re.compile(r'href *= *"([^"]*)"').findall(web_page_contents)[:10]
