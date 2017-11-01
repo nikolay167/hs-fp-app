e6c8f63a3fde87f820fe5a85a89fce10544c7e08
nikolay167 <nikolay@shityakov.com>
2017-11-01 16:01:50 +0100

    1

--- backend/__init__.py
+++ backend/__init__.py
@@ -12,7 +12,9 @@ def process_user_query(query_string):
 
 # gentleman's rule: if you opened something, and don’t need this any more, then close it!
     f.close()
-    word_apple_model = re.compile( r'<h2 class="as-bundleselection-modeltitle">\s*[A-Za-z]+ [A-Za-z]+\s*</h2>' )
+    word_apple_Computer_Model = re.compile( r'<h2 class="as-bundleselection-modeltitle">\s*[A-Za-z]+ [A-Za-z]+\s*</h2>' )
+    word_apple_model_Specs = re.compile( r'<li>\d+ </li>' )
     Applist = word_apple_model.findall(web_page_contents)
-    my_var=''
-    return my_var.append(Applist)
+    Applist1 = word_apple_model_Specs.findall(web_page_contents)
+    my_var=[]
+    return Applist1
