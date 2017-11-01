ded102e979f5ca53c9963cddd8e09e2b069b1dec
nikolay167 <nikolay@shityakov.com>
2017-11-01 18:15:56 +0100

    s

--- backend/__init__.py
+++ backend/__init__.py
@@ -13,8 +13,10 @@ def process_user_query(query_string):
 # gentleman's rule: if you opened something, and don’t need this any more, then close it!
     f.close()
     word_apple_Computer_Model = re.compile( r'<h2 class="as-bundleselection-modeltitle">\s*[A-Za-z]+ [A-Za-z]+\s*</h2>' )
-    word_apple_model_Specs = re.compile( r'<li>\d+ </li>' )
-    Applist = word_apple_model.findall(web_page_contents)
+    word_apple_model_Specs = re.compile( r' <h3 class="as-macbundle-modelvariationtitle">[1-9-A-Za-z]+ [a-z]+ [A-Za-z]+ [A-Z]+</h3>' )
+    #Applist = word_apple_model.findall(web_page_contents)
     Applist1 = word_apple_model_Specs.findall(web_page_contents)
-    my_var=[]
+    #my_var={}
+    #for n in applist
+    #    my_var[n]='charecteristics'
     return Applist1
