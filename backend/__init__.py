bca21e23f115ae537f1f95f785632a633632d195
nikolay167 <nikolay@shityakov.com>
2017-11-02 11:44:47 +0100

    e

--- backend/__init__.py
+++ backend/__init__.py
@@ -15,7 +15,7 @@ def process_user_query(query_string):
     f.close()
     #word_apple_Computer_Model = re.compile( r'<h2 class="as-bundleselection-modeltitle">\s*[A-Za-z]+ [A-Za-z]+\s*</h2>' )
     #word_apple_model_Specs = re.compile( r' <h3 class="as-macbundle-modelvariationtitle">[1-9-A-Za-z]+ [a-z]+ [A-Za-z]+ [A-Z]+</h3>' )
-    word_apple_Computer_price = re.compile( r'<span> \s*\S*\s* </span>' )
+    word_apple_Computer_price = re.compile( r'<span>\s*\S*\s*</span>' )
 
     #Applist = word_apple_model.findall(web_page_contents)
     Applist1 = word_apple_Computer_price.findall(web_page_contents)
