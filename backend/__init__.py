4f7e39c1e29c710ada31f0b60d7d24a0c19b15f8
nikolay167 <nikolay@shityakov.com>
2017-11-02 13:23:01 +0100

    e

--- backend/__init__.py
+++ backend/__init__.py
@@ -14,18 +14,20 @@ def process_user_query(query_string):
     f.close()
     word_apple_Computer_Model = re.compile( r'<h2 class="as-bundleselection-modeltitle">\s*[A-Za-z]+ [A-Za-z]+\s*</h2>' )
     word_apple_model_Specs = re.compile( r' <h3 class="as-macbundle-modelvariationtitle">[1-9-A-Za-z]+ [a-z]+ [A-Za-z]+ [A-Z]+</h3>' )
-    word_apple_Computer_price = re.compile( r'<span>\s*\S*\s*</span>' )
+    word_apple_Computer_price = re.compile( r'<span>\s*[$]\S*\s*</span>' )
 
     Applist2 = word_apple_model.findall(web_page_contents)
     Applist = word_apple_model_Specs.findall(web_page_contents)
     Applist1 = word_apple_Computer_price.findall(web_page_contents)
     my_var={}
+    return Applist1
 
-    for i in range(3):
-        my_var[Applist]=[Applist1,Applist2]
-
-
-
-    #for n in applist
-    #    my_var[n]='charecteristics'
-    return my_var
+    # for n,x,m in rang
+    #     my_var[Applist]=(Applist2,Applist)
+    #
+    # t = my_var[price]
+    # t[0]
+    # t[1]
+    #
+    # return my_var
+    #
