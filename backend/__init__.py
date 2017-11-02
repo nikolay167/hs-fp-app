83875638b0433b027d1737b09cad5e4e1af1c879
nikolay167 <nikolay@shityakov.com>
2017-11-02 19:57:54 +0100

    s
    s

--- backend/__init__.py
+++ backend/__init__.py
@@ -20,25 +20,36 @@ def process_user_query(query_string):
 
     Applist_model = word_apple_Computer_Model.findall(web_page_contents)
 
-    Applis_Specs = word_apple_model_Specs.findall(web_page_contents)
+    Applis_Specs= word_apple_model_Specs.findall(web_page_contents)
+
+
 
     Applist_price = word_apple_Computer_price.findall(web_page_contents)
 
-    clearing_from_span_Computer_model = [re.sub('\s*</?\S*\s*','',word2) for word2 in Applist_model]
+
+
+
+
+    clearing_from_span_Computer_model = [re.sub('[\n]*<[^>]*>','',word2) for word2 in Applist_model]
 
     clearing_from_span_Specs_section = [re.sub('<[^>]*?>','',word1) for word1 in Applis_Specs]
-
+    clearing_from_span_Specs_section_edited = [re.sub('<[^>]*?>','',word1) for word1 in Applis_Specs][1:]
     clearing_from_span_price_section = [re.sub('\s*</?[a-z]*>\s*','',word) for word in Applist_price]
-
-    return clearing_from_span_Specs_section
 
 
     Empty={}
+    Empty1={}
 
-    #
+
     # for i,specs in enumerate(clearing_from_span_Specs_section):
     #      Empty[clearing_from_span_Computer_model[0] + specs] = clearing_from_span_price_section[i]
-    #      return Empty
+    #
+
+    for i1,specs1 in enumerate(clearing_from_span_Specs_section_edited):
+
+         Empty1[clearing_from_span_Computer_model[0] + specs1] = clearing_from_span_price_section[1]
+    return Empty1
+
 
 
 print(process_user_query('https://www.apple.com/shop/buy-mac/mac-pro'))
