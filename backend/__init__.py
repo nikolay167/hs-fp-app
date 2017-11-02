a159578c9e56fd3307cf25edd8ff1dbaf74d2053
nikolay167 <nikolay@shityakov.com>
2017-11-02 12:14:17 +0100

    d

--- backend/__init__.py
+++ backend/__init__.py
@@ -13,12 +13,19 @@ def process_user_query(query_string):
 # gentleman's rule: if you opened something, and don’t need this any more, then close it!
     f.close()
     word_apple_Computer_Model = re.compile( r'<h2 class="as-bundleselection-modeltitle">\s*[A-Za-z]+ [A-Za-z]+\s*</h2>' )
-    #word_apple_model_Specs = re.compile( r' <h3 class="as-macbundle-modelvariationtitle">[1-9-A-Za-z]+ [a-z]+ [A-Za-z]+ [A-Z]+</h3>' )
-    #word_apple_Computer_price = re.compile( r'<span>\s*\S*\s*</span>' )
+    word_apple_model_Specs = re.compile( r' <h3 class="as-macbundle-modelvariationtitle">[1-9-A-Za-z]+ [a-z]+ [A-Za-z]+ [A-Z]+</h3>' )
+    word_apple_Computer_price = re.compile( r'<span>\s*\S*\s*</span>' )
 
-    #Applist = word_apple_model.findall(web_page_contents)
-    Applist1 = word_apple_Computer_Model.findall(web_page_contents)
-    #my_var={}
+    Applist2 = word_apple_model.findall(web_page_contents)
+    Applist = word_apple_model_Specs.findall(web_page_contents)
+    Applist1 = word_apple_Computer_price.findall(web_page_contents)
+    my_var={}
+
+    for i in range(3):
+        my_var[Applist]=[Applist1,Applist2]
+
+
+
     #for n in applist
     #    my_var[n]='charecteristics'
-    return Applist1
+    return my_var
